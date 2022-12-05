a15f30557951739c969a4085cc04efeac390c5dd
Luigi9411 <luigi.sciavicco@gmail.com>
2022-12-05 17:29:53 +0100

    first-commit

---
+++ src/components/HeaderPage.vue
@@ -0,0 +1,49 @@
+<template>
+   <header>
+      <div class="container">
+         <ul>
+          <li>Everything about Lifestyle, Travel and Godgets!</li>
+          <li>SIGN IN</li>
+          <li>ABOUT US</li>
+          <li>CONTACT US</li>
+          <li>BUY NOW</li>
+         </ul>
+      </div>
+      <div class="logo">
+
+      </div>
+   </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderPage',
+};
+</script>
+
+<style lang="scss" scoped>
+  header{
+    .container{
+      width: 1100px;
+      margin: 0 auto;
+      background-color: #0088CC;
+      height: 5vh;
+      ul{
+        display: flex;
+        height: 5vh;
+        gap: 1em;
+        align-items: center;
+        list-style-type: none;
+        color: white;
+        font-size: .6em;
+        li{
+           cursor: pointer;
+        }
+      }
+    }
+    .logo{
+      height: 15vh;
+
+    }
+  }
+</style>
